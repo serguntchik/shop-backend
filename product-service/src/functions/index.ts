8ff96e50d99ec4dca5e23caba481484dab8bbb72
Sergey Volkov <fizteh.volkov@gmail.com>
2021-10-17 12:48:41 +0300

    feat(bff): product edit and delete modules have been added

--- product-service/src/functions/index.ts
+++ product-service/src/functions/index.ts
@@ -2,3 +2,5 @@ export { default as catalogBatchProcess } from './catalog-batch-process';
 export { default as products } from './products-list';
 export { default as getProductById } from './product-item';
 export { default as createProduct } from './product-create';
+export { default as editProduct } from './product-edit';
+export { default as deleteProduct } from './product-delete';
---
+++ product-service/src/functions/product-edit/handler.spec.ts
@@ -0,0 +1,41 @@
+import { prismaMock } from '../../prisma/client.mock';
+
+import { main } from '@functions/product-create/handler';
+
+import { Product } from '.prisma/client';
+
+import { ProductResponse } from "../../core/product.interface";
+import { MOCK_CONTEXT } from '../../core/testing';
+
+it('should create a new product', async () => {
+    const product: ProductResponse = {
+        id: '7567ec4b-b10c-48c5-9345-fc73c48a80a0',
+        title: 'Mazda',
+        description: 'A splendid Mazda auto',
+        price: 154,
+        count: 67,
+    };
+    const resolvedProduct: Product & { stocks: { count: number } } = {
+        id: product.id!,
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        stocks: {
+            count: product.count,
+        },
+    };
+    const event = {
+        body: {
+            title: product.title,
+            description: product.description,
+            price: product.price,
+            count: product.count,
+        },
+    };
+
+    prismaMock.product.create.mockResolvedValue(resolvedProduct);
+    const response: { body: string, statusCode: number } = await main(event, MOCK_CONTEXT, null!);
+
+    expect(response.statusCode).toBe(201);
+    expect(JSON.parse(response.body)).toStrictEqual(product);
+});
